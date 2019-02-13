548df9491100f8748326174f5798a31f31976327
root <root@localhost.localdomain>
2019-02-13 14:29:37 +0100

    update homepage response

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).json('Hello world!');
+    res.status(200).json('hi');
 });
 
 app.get('/users', (req, res) => {
