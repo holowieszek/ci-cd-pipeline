74ebe4b66c4da2a54a461a5b1e3b62077cf57888
root <root@localhost.localdomain>
2019-02-13 17:10:21 +0100

    updated response message on / GET

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).json('Hey world!');
+    res.status(200).json('Hello world again!');
 });
 
 app.get('/users', (req, res) => {
